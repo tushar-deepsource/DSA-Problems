68789910e3ef39f05b7e75e0d3f4a84d882dcf8c
Narayan soni <77617189+narayan954@users.noreply.github.com>
2022-05-06 01:20:05 +0530

    Create 73. Set Matrix Zeroes.py

---
+++ leetcode/73. Set Matrix Zeroes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        m = len(matrix)
+        n = len(matrix[0])
+        locations = []
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    locations.append([i,j])
+        for location in locations:
+            for i in range(m):
+                matrix[i][location[1]]=0
+            for j in range(n):
+                matrix[location[0]][j]=0
