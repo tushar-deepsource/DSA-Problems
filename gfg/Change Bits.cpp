7c669484234c64bec985e96b01be9134a1bbe6f2
Narayan soni <77617189+narayan954@users.noreply.github.com>
2022-06-30 14:58:42 +0530

    Create Change Bits.cpp

---
+++ gfg/Change Bits.cpp
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    vector<int> changeBits(int N) {
+        vector<int> ans;
+        int num = log2(N)+1;
+        num = pow(2,num)-1;
+        int diff = num - N;
+        ans.push_back(diff);
+        ans.push_back(num);
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        Solution ob;
+        auto ans = ob.changeBits(N);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
