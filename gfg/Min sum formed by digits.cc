22529d0a2725b9320cc9d2c3a41b97a0be456f33
Narayan soni <77617189+narayan954@users.noreply.github.com>
2022-07-15 03:01:04 +0530

    Create Min sum formed by digits.cc

---
+++ gfg/Min sum formed by digits.cc
@@ -0,0 +1,38 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution{
+    public:
+    long long int minSum(int arr[], int n)
+    {
+        sort(arr, arr+n);
+        long long num1 = 0, num2 = 0;
+        for(int i=0; i<n; i++){
+            if(i%2) num1 = num1*10 + arr[i];
+            else num2 = num2*10 + arr[i];
+        }
+        return num1+num2;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n+1];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        cout<<ob.minSum(arr, n)<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
